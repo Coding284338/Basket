ca9bfe9fe18f218fe26eb582a454918add20868d
Coding284338 <86387056+Coding284338@users.noreply.github.com>
2021-08-27 18:06:42 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var playerCount = 0
+var allPlayer
+var gameState
+//load the images of background, player,fruit1 - fruit5
+function preload(){
+  
+}
+function setup() {
+  createCanvas(1000, 600);
+  
+  
+}
+
+function draw() {
+  background(0)
+  
+   
+}
